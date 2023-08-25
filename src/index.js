b37436b3c09a9475fc5af946cf677a70c1e5edf7
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-25 10:07:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 // axios.defaults.headers.common['x-api-key'] =
 //   'live_SG6chukU8EBugubL4otSUjuN1o5sFWslgv4YLx7Gm782NeUW5OYBEnbcWOImIoPQ';
