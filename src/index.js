cb20143b34e3718a5a7a8a330f5b30bbad87fdae
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-09-03 22:50:24 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,50 +1,62 @@
-// import SlimSelect from 'slim-select';
-// new SlimSelect({
-//   select: '#selectElement',
-//   // settings: {
-//   //   contentPosition: 'relative',
-//   //   contentLocation: document.getElementById('local'),
-//   // },
-// });
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
+const loader = document.querySelector('.loader');
 const select = document.querySelector('.breed-select');
 const fotoDiv = document.querySelector('.cat-info');
-const spanStyle = document.querySelector('.span');
+const load = document.querySelector('.load');
+select.setAttribute('id', 'selectElement');
 
 const options = {
-  method: 'GET',
-  api_key:
+  BASE_URL: 'https://api.thecatapi.com/v1',
+  END_POINT_BREEDS: '/breeds',
+  END_POINT_IMG: '/images/',
+  API_KEY:
     'live_SG6chukU8EBugubL4otSUjuN1o5sFWslgv4YLx7Gm782NeUW5OYBEnbcWOImIoPQ',
-  breed_ids: select.value,
 };
-fetch('https://api.thecatapi.com/v1/breeds')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
 
-    return response.json();
-  })
+function fetchBreeds() {
+  fetch(`${options.BASE_URL}${options.END_POINT_BREEDS}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
 
-  .then(data => {
-    select.insertAdjacentHTML('afterbegin', createMarkup(data));
-  })
-  .catch(err => {
-    console.log(err);
-  });
+      return response.json();
+    })
+
+    .then(response => {
+      select.insertAdjacentHTML('afterbegin', createMarkup(response));
+      new SlimSelect({
+        select: '#selectElement',
+      });
+    })
+    .catch(error => {
+      Notiflix.Notify.warning(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    })
+    .finally(() => {
+      Notiflix.Notify.info('Loading data, please wait...');
+    });
+}
 
 function createMarkup(arr) {
   return arr
     .map(({ id, name }) => `<option value="${id}">${name}</option>`)
     .join('');
 }
+fetchBreeds();
+select.addEventListener('change', fetchCatByBreed);
 
-select.addEventListener('change', currentCats);
+function fetchCatByBreed() {
+  const breedId = select.value;
+  loader.classList.remove('is-hidden');
+  fotoDiv.classList.add('is-hidden');
 
-function currentCats(evt) {
-  const breed = select.value;
-  fetch(
-    `https://api.thecatapi.com/v1/images/search?breed_ids=${breed}&api_key=live_SG6chukU8EBugubL4otSUjuN1o5sFWslgv4YLx7Gm782NeUW5OYBEnbcWOImIoPQ`
+  return fetch(
+    `${options.BASE_URL}${options.END_POINT_IMG}search?breed_ids=${breedId}&api_key=${options.API_KEY}`
   )
     .then(response => {
       if (!response.ok) {
@@ -55,6 +67,16 @@ function currentCats(evt) {
     })
     .then(data => {
       fotoDiv.innerHTML = createMarkup3(data);
+    })
+    .catch(error => {
+      Notiflix.Notify.warning(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    })
+    .finally(() => {
+      Notiflix.Notify.info('Loading data, please wait...');
+      loader.classList.add('is-hidden');
+      fotoDiv.classList.remove('is-hidden');
     });
 }
 
@@ -66,7 +88,7 @@ function createMarkup3(arr) {
         <div class="text">
     <h2>${breeds.map(item => item.name)}</h2>
     <p>${breeds.map(item => item.description)}</p>
-    <p><span class="span">Temperament: </span>${breeds.map(
+    <p><h3 class="s">Temperament: </h3>${breeds.map(
       item => item.temperament
     )}</p>
   </div>`
@@ -74,11 +96,8 @@ function createMarkup3(arr) {
     .join('');
 }
 
-select.setAttribute('id', 'selectElement');
-
 // styles
-
+load.style.textAlign = 'center';
 fotoDiv.style.display = 'flex';
 fotoDiv.style.gap = '15px';
 fotoDiv.style.marginTop = '15px';
-// spanStyle.style.fontWeight = '600';
